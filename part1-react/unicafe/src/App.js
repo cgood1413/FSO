70000f9f55d993bf7425b63121a8ea3ae847bdcf
Chance Goodwin <cgood1413@gmail.com>
2022-06-30 16:20:05 -0500

    Exercises 1.10 & 1.11 Completion

--- part1-react/unicafe/src/App.js
+++ part1-react/unicafe/src/App.js
@@ -1,52 +1,58 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Button = (props) => (
-  <button onClick={props.onClick}>{props.text}</button>
-)
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
-const Statistics = (props) => {
-  return (
-    <div>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p><strong>All: {props.total}</strong></p>
-      <p><strong>Average: {props.average || 0}</strong></p>
-      <p><strong>Positive: {props.positive || 0}%</strong></p>
-    </div>
-  );
-}
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}:</td> <td>{props.value}</td>
+  </tr>
+);
+
+const Statistics = (props) => (
+  <table>
+    <StatisticLine text={"Good"} value={props.good} />
+    <StatisticLine text={"Neutral"} value={props.neutral} />
+    <StatisticLine text={"Bad"} value={props.bad} />
+    <StatisticLine text={"Total"} value={props.total} />
+    <StatisticLine text={"Average"} value={props.average} />
+    <StatisticLine text={"Positive"} value={props.positive} />
+  </table>
+);
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   const total = good + neutral + bad;
-  const average = (good * 1) + (bad * -1) / total;
+  const average = good * 1 + (bad * -1) / total;
   const positive = (good / total) * 100;
 
   const handleClick = (state, setter) => () => {
     setter(state + 1);
-  }
-
-  // const getAverage = () => {
-  //   const totalScore = ((good * 1) + (bad * -1));
-  //   return totalScore / totalCount;
-  // }
+  };
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button text="good" onClick={handleClick(good, setGood)}/>
+      <Button text="good" onClick={handleClick(good, setGood)} />
       <Button text="neutral" onClick={handleClick(neutral, setNeutral)} />
-      <Button text="bad" onClick={handleClick(bad, setBad)}/>
+      <Button text="bad" onClick={handleClick(bad, setBad)} />
       <h1>Statistics</h1>
-      {total 
-      ? <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/> 
-      : <p>No feedback given yet...</p>}
+      {total ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          average={average}
+          positive={positive}
+        />
+      ) : (
+        <p>No feedback given yet...</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
